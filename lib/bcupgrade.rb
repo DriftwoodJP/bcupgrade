87edf94048f5c568b9102a88c9a1afe4cf476a3f
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2016-09-12 18:52:30 +0900

    Fix: adjust new spec(brew cask info)

--- lib/bcupgrade.rb
+++ lib/bcupgrade.rb
@@ -1,5 +1,16 @@
 require 'bcupgrade/version'
 
 module Bcupgrade
-  # Your code goes here...
+  def self.check_update(casks)
+    update_casks = []
+    casks.each do |cask|
+      info = `brew cask info #{cask}`
+      lines = info.split(/\n/)
+      version = lines[0].gsub(/.+: (.+)/, '\1')
+
+      installed_path = "/usr/local/Caskroom/#{cask}/#{version}"
+      update_casks.push(cask) unless info.include?(installed_path)
+    end
+    update_casks
+  end
 end
--- spec/bcupgrade_spec.rb
+++ spec/bcupgrade_spec.rb
@@ -1,11 +1,35 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Bcupgrade do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Bcupgrade::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'brew cask info' do
+    let(:app_name) { 'atom' }
+    let(:cask_info) { `brew cask info #{app_name}` }
+    let(:first_line) {
+      lines = cask_info.split(/\n/)
+      lines[0]
+    }
+    let(:version) { first_line.gsub(/.+: (.+)/, '\1') }
+
+    it 'has a app name' do
+      expect(first_line).to match(/#{app_name}: /)
+    end
+
+    it 'has a version number' do
+      expect(first_line).to match(version)
+    end
+
+    it 'has install paths' do
+      expect(cask_info).to include("/usr/local/Caskroom/#{app_name}/")
+    end
   end
+
+  # describe '#check_update' do
+  #   it 'returns Array' do
+  #     expect(Bcupgrade.check_update(['atom'])).to eq(['atom'])
+  #   end
+  # end
 end
