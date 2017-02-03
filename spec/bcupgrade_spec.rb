7f1a4110a4d1f603a36b9077d2135d7401836689
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-02-03 13:33:41 +0900

    Clean: rubocop

--- spec/bcupgrade_spec.rb
+++ spec/bcupgrade_spec.rb
@@ -8,7 +8,7 @@ describe Bcupgrade do
   describe '#brew_cask_info' do
     let(:installed_path) { Bcupgrade::CASKROOM_PATH }
     let(:app_name) { 'atom' }
-    let(:cask_info) { Bcupgrade.brew_cask_info(app_name) }
+    let(:cask_info) { described_class.brew_cask_info(app_name) }
     let(:first_line) do
       lines = cask_info.split(/\n/)
       lines[0]
@@ -29,9 +29,9 @@ describe Bcupgrade do
   end
 
   describe '#brew_cask_list' do
-    let(:cask_list) { Bcupgrade.brew_cask_list }
+    let(:cask_list) { described_class.brew_cask_list }
 
-    it 'should be a kind of String' do
+    it 'has a kind of String' do
       expect(cask_list).to be_kind_of(String)
     end
   end
@@ -39,70 +39,70 @@ describe Bcupgrade do
   describe '#check_list' do
     before do
       output = "atom (!)\n1password\nactprinter\nalfred\n"
-      allow(Bcupgrade).to receive(:brew_cask_list).and_return(output)
+      allow(described_class).to receive(:brew_cask_list).and_return(output)
     end
 
-    it 'should be a kind of Array' do
-      expect(Bcupgrade.check_list).to be_kind_of(Array)
+    it 'has a kind of Array' do
+      expect(described_class.check_list).to be_kind_of(Array)
     end
 
-    it 'should return "installed_casks" and "error_casks"' do
-      expect(Bcupgrade.check_list).to eq([%w(1password actprinter alfred), %w(atom)])
+    it 'returns "installed_casks" and "error_casks"' do
+      expect(described_class.check_list).to eq([%w(1password actprinter alfred), %w(atom)])
     end
 
-    it 'should not include "(!)"' do
-      expect(Bcupgrade.check_list.to_s).not_to include('(!)')
+    it 'has not include "(!)"' do
+      expect(described_class.check_list.to_s).not_to include('(!)')
     end
   end
 
   describe '#check_version' do
     context 'When raise error "Error: File /Users/***/*** is not a plain file"' do
-      it 'should return "error"' do
+      it 'returns "error"' do
         brew_cask_info_error = ''
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(brew_cask_info_error)
-        expect(Bcupgrade.check_version('dropbox')).to eq('error')
+        allow(described_class).to receive(:brew_cask_info).and_return(brew_cask_info_error)
+        expect(described_class.check_version('dropbox')).to eq('error')
       end
     end
 
     context 'When the latest version is installed,' do
-      it 'should return "nil"' do
+      it 'returns "nil"' do
         output = File.read('spec/factories/brew_cask_info_atom.txt')
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(output)
-        expect(Bcupgrade.check_version('atom')).to eq(nil)
+        allow(described_class).to receive(:brew_cask_info).and_return(output)
+        expect(described_class.check_version('atom')).to eq(nil)
       end
     end
 
     context 'When the latest version is not installed,' do
-      it 'should return "version" string' do
+      it 'returns "version" string' do
         output = File.read('spec/factories/brew_cask_info_android-studio.txt')
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(output)
-        expect(Bcupgrade.check_version('android-studio')).to be_kind_of(String)
+        allow(described_class).to receive(:brew_cask_info).and_return(output)
+        expect(described_class.check_version('android-studio')).to be_kind_of(String)
       end
     end
 
     context 'When the previous version is installed,' do
-      example '(6.3.1) should return "6.3.2"' do
+      example '(6.3.1) returns "6.3.2"' do
         output = File.read('spec/factories/brew_cask_info_1password.txt')
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(output)
-        expect(Bcupgrade.check_version('atom')).to eq('6.3.2')
+        allow(described_class).to receive(:brew_cask_info).and_return(output)
+        expect(described_class.check_version('atom')).to eq('6.3.2')
       end
 
-      example '(2.1.2.0,143.2915827) should return "2.1.3.0,143.3101438"' do
+      example '(2.1.2.0,143.2915827) returns "2.1.3.0,143.3101438"' do
         output = File.read('spec/factories/brew_cask_info_android-studio.txt')
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(output)
-        expect(Bcupgrade.check_version('android-studio')).to eq('2.1.3.0,143.3101438')
+        allow(described_class).to receive(:brew_cask_info).and_return(output)
+        expect(described_class.check_version('android-studio')).to eq('2.1.3.0,143.3101438')
       end
 
-      example '(3.0.3_694) should return "3.1_718"' do
+      example '(3.0.3_694) returns "3.1_718"' do
         output = File.read('spec/factories/brew_cask_info_alfred.txt')
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(output)
-        expect(Bcupgrade.check_version('alfred')).to eq('3.1_718')
+        allow(described_class).to receive(:brew_cask_info).and_return(output)
+        expect(described_class.check_version('alfred')).to eq('3.1_718')
       end
 
-      example '(latest) should return "nil"' do
+      example '(latest) returns "nil"' do
         output = File.read('spec/factories/brew_cask_info_betterzipql.txt')
-        allow(Bcupgrade).to receive(:brew_cask_info).and_return(output)
-        expect(Bcupgrade.check_version('betterzipql')).to eq(nil)
+        allow(described_class).to receive(:brew_cask_info).and_return(output)
+        expect(described_class.check_version('betterzipql')).to eq(nil)
       end
     end
   end
