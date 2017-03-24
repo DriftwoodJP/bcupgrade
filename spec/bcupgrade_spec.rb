c63dc954deaead08438faea5adca1ef878389e07
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-03-24 19:29:29 +0900

    Modify: separates the specs into some files

--- spec/bcupgrade_spec.rb
+++ spec/bcupgrade_spec.rb
@@ -1,41 +1,6 @@
 require 'spec_helper'
 
 describe Bcupgrade do
-  it 'has a version number' do
-    expect(Bcupgrade::VERSION).not_to be(nil)
-  end
-
-  describe '#brew_cask_info' do
-    let(:installed_path) { Bcupgrade::CASKROOM_PATH }
-    let(:app_name) { 'atom' }
-    let(:cask_info) { described_class.brew_cask_info(app_name) }
-    let(:first_line) do
-      lines = cask_info.split(/\n/)
-      lines[0]
-    end
-    let(:latest_version) { first_line.gsub(/.+: (.+)/, '\1') }
-
-    it 'has a app name of cask' do
-      expect(first_line).to match(/#{app_name}: /)
-    end
-
-    it 'has a latest version number of cask' do
-      expect(first_line).to match(latest_version)
-    end
-
-    it 'has installed paths of cask' do
-      expect(cask_info).to include("#{installed_path}/#{app_name}/")
-    end
-  end
-
-  describe '#brew_cask_list' do
-    let(:cask_list) { described_class.brew_cask_list }
-
-    it 'has a kind of String' do
-      expect(cask_list).to be_kind_of(String)
-    end
-  end
-
   describe '#check_list' do
     before do
       output = "atom (!)\n1password\nactprinter\nalfred\n"
